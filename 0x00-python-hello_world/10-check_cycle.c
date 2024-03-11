fa11288af8e63cfb8aceb3cd7d9e8c5c35d6edc2
mornark <chrixiants@gmail.com>
2024-03-11 21:54:55 +0300

    function to detect if list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Function to check if cycle exists in a list
+ * @list: pointer to head of linked_list
+ * Return: 0 if cycle exists, 1 if it doesn't
+ **/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp1, *temp2;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	temp1 = temp2 = list;
+	while (temp1 != NULL)
+	{
+		temp1 = temp1->next;
+		if (temp1 == NULL)
+			return (0);
+		temp2 = temp2->next;
+		temp1 = temp1->next;
+		if (temp1 == temp2)
+			return (1);
+	}
+
+	return (0);
+}
---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+s1 = " is better than "
+s2 = "If the implementation is "
+s3 = " to explain, "
+print(f'''\
+The Zen of Python, by Tim Peters
+
+Beautiful{s1}ugly.
+Explicit{s1}implicit.
+Simple{s1}complex.
+Complex{s1}complicated.
+Flat{s1}nested.
+Sparse{s1}dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+{s2}hard{s3}it's a bad idea.
+{s2}easy{s3}it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+''')
