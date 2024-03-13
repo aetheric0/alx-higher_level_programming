13fac322993b67f8155738908f619f12c220e00e
mornark <chrixiants@gmail.com>
2024-03-13 03:16:46 +0300

    ord('z') incremented by 1 to accomodate z

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for i in range(len(str)):
         a = ord(str[i])
-        if a in range(ord('a'), ord('z')):
+        if a in range(ord('a'), ord('z')+1):
             a = a - 32
         print("{}".format(chr(a)), end="")
     print()
