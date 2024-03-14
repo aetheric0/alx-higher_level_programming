35ec0244d9aac7285c0730bcc094aeb4d695656d
mornark <chrixiants@gmail.com>
2024-03-14 16:54:02 +0300

    format() used instead for when one argument is passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     from sys import argv
     for i in range(len(argv)):
         if len(argv) == 2 and i == 1:
-            print("1 argument: ")
-            print(f"1: {argv[i]}")
+            print("{} argument: ".format(i))
+            print("{}: {}".format(i, argv[i]))
         elif len(argv) < 2:
             print("0 arguments.")
         elif len(argv) > 2:
