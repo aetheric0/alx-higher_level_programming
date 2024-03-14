a52ac0175925b475c49cdd3f0a19e8866ad6e3e2
mornark <chrixiants@gmail.com>
2024-03-14 10:40:56 +0300

    importing modules with __import__ exception

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Import function add(a, b) from module add_0 and prints
+# addition of arguments passed to imported function
+# Would not execute if __import__ is used
+
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(1, 2) ))
