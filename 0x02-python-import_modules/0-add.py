65439e3bd32f693463476d73b002dec253af136f
mornark <chrixiants@gmail.com>
2024-03-14 15:45:22 +0300

    comments and extra lines removed coz i don't know

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-
-# Import function add(a, b) from module add_0 and prints
-# addition of arguments passed to imported function
-# Would not execute if __import__ is used
-
-
 a = 1
 b = 2
-
 from add_0 import add
 print("{} + {} = {}".format(a, b, add(a, b)))
