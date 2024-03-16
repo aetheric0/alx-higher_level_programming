ebcbb01948fa2af543032cc3aae99d5f1e968ee1
mornark <chrixiants@gmail.com>
2024-03-16 19:42:44 +0300

    palindrome detected!

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -3,72 +3,33 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * main - check the code for
+ *
+ * Return: Always 0.
  */
-size_t print_listint(const listint_t *h)
+int main(void)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+    listint_t *head;
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+    head = NULL;
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 50);
+    add_nodeint_end(&head, 972);
+    add_nodeint_end(&head, 17);
+    add_nodeint_end(&head, 1);
+    print_listint(head);
 
-    return (n);
-}
-
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-    listint_t *new;
-    listint_t *current;
-
-    current = *head;
-
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
-
-    new->n = n;
-    new->next = NULL;
-
-    if (*head == NULL)
-        *head = new;
+    if (is_palindrome(&head) == 1)
+        printf("Linked list is a palindrome\n");
     else
-    {
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new;
-    }
+        printf("Linked list is not a palindrome\n");
 
-    return (new);
-}
+    free_listint(head);
 
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+    return (0);
 }
