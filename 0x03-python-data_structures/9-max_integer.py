5bf73370d73509f1e11fbaf6259924cc1c614fbe
mornark <chrixiants@gmail.com>
2024-03-16 17:48:15 +0300

    condition for empty list added

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     a = 0
+    if my_list == []:
+        return None
     for i in range(len(my_list) - 1):
         if a > my_list[i]:
             continue
