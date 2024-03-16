c6d025fca57dfb49319da1e37e2b59b067d8a7b6
mornark <chrixiants@gmail.com>
2024-03-16 17:49:40 +0300

    length of list adjusted to accomodate all values

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     a = 0
     if my_list == []:
         return None
-    for i in range(len(my_list) - 1):
+    for i in range(len(my_list)):
         if a > my_list[i]:
             continue
         else:
