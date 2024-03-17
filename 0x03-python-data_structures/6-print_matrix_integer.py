7e19f8493cd38033970c3b632b108ae7c1ca3be1
mornark <chrixiants@gmail.com>
2024-03-17 21:13:43 +0300

    str.format() used for integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for num in matrix:
         for i in range(len(num)):
             if num[i] == num[-1]:
-                print("{}".format(num[i]), end="")
+                print("{:d}".format(num[i]), end="")
             else:
-                print("{} ".format(num[i]), end="")
+                print("{:d} ".format(num[i]), end="")
         print()
