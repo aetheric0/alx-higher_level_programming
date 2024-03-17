e7d3e3b10e937217059654b74cac9cdad0b637bf
mornark <chrixiants@gmail.com>
2024-03-17 21:21:07 +0300

    check for empty list placed first

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    a = my_list[0]
     if my_list == []:
         return None
+    a = my_list[0]
     for i in range(len(my_list)):
         if a > my_list[i]:
             continue
