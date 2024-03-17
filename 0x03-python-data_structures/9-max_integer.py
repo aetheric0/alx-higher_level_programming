5152fdc660a755d5353e022fc792994bdfa2c725
mornark <chrixiants@gmail.com>
2024-03-17 21:15:52 +0300

    condition added for if list exists

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    a = 0
-    if my_list == []:
-        return None
-    for i in range(len(my_list)):
-        if a > my_list[i]:
-            continue
-        else:
-            a = my_list[i]
+    if my_list:
+        a = 0
+        if my_list == []:
+            return None
+        for i in range(len(my_list)):
+            if a > my_list[i]:
+                continue
+            else:
+                a = my_list[i]
     return a
