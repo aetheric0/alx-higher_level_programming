6b48e8639dc7234ade71250807f13cb87b8dcdca
mornark <chrixiants@gmail.com>
2024-03-17 21:20:15 +0300

    a first initialized to first member of list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-        a = 0
-        if my_list == []:
-            return None
-        for i in range(len(my_list)):
-            if a > my_list[i]:
-                continue
-            else:
-                a = my_list[i]
+    a = my_list[0]
+    if my_list == []:
+        return None
+    for i in range(len(my_list)):
+        if a > my_list[i]:
+            continue
+        else:
+            a = my_list[i]
     return a
