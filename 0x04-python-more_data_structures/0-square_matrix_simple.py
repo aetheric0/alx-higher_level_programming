d08d9c57cba70083ef7149125e3479d5289db62e
mornark <chrixiants@gmail.com>
2024-03-21 04:07:29 +0300

    list within list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
+    row = []
     for num in matrix:
         for i in num:
             i = i * i
-            new_matrix.append(i)
+            row.append(i)
+        new_matrix.append(row)
+        row = []
     return new_matrix
