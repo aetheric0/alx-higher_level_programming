b152db1ab8dc49988609e9444fb80aee8975b8a9
mornark <chrixiants@gmail.com>
2024-04-02 16:38:50 +0300

    pycodestyle verified

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for item in my_list:
             if i < x:
-                print("{}".format(item), end = "")
+                print("{}".format(item), end="")
                 i += 1
         print()
         return (i)
-    except:
+    except Exception:
         print("Error: Could not access list")
