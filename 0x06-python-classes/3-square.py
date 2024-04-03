022c158d146d9e83696a9cd40201f7dc60d99018
mornark <chrixiants@gmail.com>
2024-04-03 04:45:05 +0300

    self marrying self

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,6 +4,7 @@
 
 
 class Square:
+    """ private instance attribute with validation"""
     def __init__(self, size=0):
         if isinstance(size, int):
             if size >= 0:
@@ -12,3 +13,7 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
+
+    def area(self):
+        self.__size *= self.__size
+        return self.__size
