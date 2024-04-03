9f403b243fe05998db4173aba8cdb39f21c20768
mornark <chrixiants@gmail.com>
2024-04-03 04:28:51 +0300

    private square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Creates a Class Square with Private Attribute Size"""
+
+
+class Square:
+    """ Creates and Instantiate Private Attribute Size"""
+    def __init__(self, size):
+        self.__size = size
