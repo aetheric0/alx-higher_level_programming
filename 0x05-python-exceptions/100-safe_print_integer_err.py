b8e184f968e1f2efe83b1b27d4781a39082984e0
mornark <chrixiants@gmail.com>
2024-04-03 21:55:07 +0300

    exception handled

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import sys
+from sys import stderr
 
 def safe_print_integer_err(value):
     try:
-        if isinstance(value, int):
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except Exception as err:
-        sys.stderr.write("Exception:", err)
+        print("Exception:", err, file=stderr)
---
+++ 0x05-python-exceptions/exception.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def Makanaki():
+    try:
+        a = 5 + "obolo"*[]
+    except Exception as err:
+        print("Exception: ", err)
+Makanaki()
