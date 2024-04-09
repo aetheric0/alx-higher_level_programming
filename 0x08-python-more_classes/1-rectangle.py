0f46929e204b6c4b78742f8006066d50e4633e20
mornark <chrixiants@gmail.com>
2024-04-09 12:25:49 +0300

    Rectangle class with width and height instantiatiotter property attributes are also defined

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+The module 1-Rectangle which returns/sets the width and height
+"""
+
+
+class Rectangle:
+    """
+    The Rectangle class
+    ====================
+
+    Defines class getter and setter attributes that retrieves or sets
+    the width and height of the class
+    """
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
