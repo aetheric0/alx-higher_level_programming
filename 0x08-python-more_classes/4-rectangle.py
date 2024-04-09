423f56d15969444ef04ca2777fa920d2894e29e0
mornark <chrixiants@gmail.com>
2024-04-09 17:11:44 +0300

    repr implemented

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -66,3 +66,11 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2*(self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width] * self.height)
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
