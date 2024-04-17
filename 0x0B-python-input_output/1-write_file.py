9a86b7f328a2367cafb628a25c4836f026d7a37d
mornark <chrixiants@gmail.com>
2024-04-17 17:17:44 +0300

    function corrected

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,12 +5,12 @@ Contains write_file function that writes to a file
 """
 
 
-def write_file(filename=""):
+def write_file(filename="", text=""):
     """
     Function that writes to filename
     Returns the numbers of characters written
     """
 
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, mode="w", encoding="utf-8") as file:
         a = file.write(text)
-        return a
+    return a
