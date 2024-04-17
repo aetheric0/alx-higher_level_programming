8328d2e4a57d1c8524a4600c48733072060093d5
mornark <chrixiants@gmail.com>
2024-04-17 17:34:08 +0300

    json serialization function

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module to serialize objects to json
+"""
+
+import json
+
+def to_json_string(my_obj):
+    """
+    function to serialize my_obj to json representation
+    """
+    return json.dumps(my_obj)
