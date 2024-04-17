cd99e26aa82cc925d0d35ad6900ca3f250b7d50e
mornark <chrixiants@gmail.com>
2024-04-17 15:08:06 +0300

    function that reads a file and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Documentation for 0-read_file module
+Contains read_file function that reads a file to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Function that reads a file
+    """
+
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
