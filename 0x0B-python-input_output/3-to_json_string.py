d892c65186a3d22b135a5379ed44f00b117f361f
mornark <chrixiants@gmail.com>
2024-04-17 17:43:43 +0300

    pycodestyle verified

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,8 +5,10 @@ Module to serialize objects to json
 
 import json
 
+
 def to_json_string(my_obj):
     """
     function to serialize my_obj to json representation
     """
+
     return json.dumps(my_obj)
