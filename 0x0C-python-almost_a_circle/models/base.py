8fe96785657a580824c415d79fd0d580035f4a87
mornark <chrixiants@gmail.com>
2024-04-23 01:14:16 +0300

    serialization in classmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,14 @@ class Base():
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + '.json'
+        with open(filename, mode='w', encoding='utf-8') as file1:
+            if list_objs is None:
+                file1.write('[]')
+                return
+            else:
+                objs = [obj.to_dictionary() for obj in list_objs]
+                file1.write(cls.to_json_string(objs))
