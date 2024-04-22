c7fce394af331c97d87eef15d61c7a536c993a78
mornark <chrixiants@gmail.com>
2024-04-22 22:17:45 +0300

    11 done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,18 @@ class Square(Rectangle):
     def __str__(self):
         return '[Square] ({}) {}/{} - {}]'.format(self.id, self.x,
                                                   self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value > 0:
+                self.width = value
+                self.height = value
+            else:
+                raise ValueError('width must be > 0')
+        else:
+            raise TypeError('width must be an integer')
