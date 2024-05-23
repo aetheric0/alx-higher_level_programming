591e1cd54efdfdd9df97b910107ccc842f0eb154
root <root@Preston21>
2024-05-23 20:59:10 +0300

    wrong value corrected

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
 -- run using: cat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 
 -- shows tables in a database passed as an argument from tty
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, 'John', 10), (2, 'Alex', 3), (2, 'Bob', 14), (4, 'George', 8);
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
