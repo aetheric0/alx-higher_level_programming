2cda159875097604aca51b66b305e6e53866f58b
root <root@Preston21>
2024-05-23 11:19:25 +0300

    other scripts

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- run using <script-name.sql> | mysql -hlocalhost -uroot -p <database-name>
+-- shows tables in a database passed as an argument from tty
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- run using <script-name.sql> | mysql -hlocalhost -uroot -p <database-name>
 -- shows tables in a database passed as an argument from tty
-SHOW TABLES from %s
+ INSERT INTO first_table VALUES (id=89, name='Best School');
