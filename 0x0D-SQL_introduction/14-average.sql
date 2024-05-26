d93107da65eff26160c062db31d72e2505ad6997
monarch <monarchofaether@gmail.com>
2024-05-26 16:25:35 +0300

    column renamed to average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- run the command using cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
 -- Returns the number of rows according the condition from a table
-SELECT AVG(`score`) FROM second_table;
+SELECT AVG(`score`) AS average FROM second_table;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- run the command using cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
 -- Returns the number of rows according the condition from a table
-SELECT `score`, `name` FROM second_table ORDER BY `score` DESC;
+SELECT `score`, `name` FROM second_table WHERE `score` > 5;
