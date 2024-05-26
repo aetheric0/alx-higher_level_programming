4d1f803945b3e565b3f5f0488bf95b4870527123
monarch <monarchofaether@gmail.com>
2024-05-26 20:14:55 +0300

    Creates a database and user with on SELECT privilege on it

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- SHOWS privileges for user_0d_1 and user_0d_2
+-- Run with command:
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
