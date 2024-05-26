be703dcb4d018e3401128065ab76acd4d5849b46
Monarch <monarchofaether@gmail.com>
2024-05-26 18:54:52 +0300

    removed CHARACTER SET from ALTER TABLE statement

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- run with cmd line: cat 100-move_to_utf8.sql | mysql -hlocalhost -uroot -p
 -- ALTERS DATABASE encoding to UTF-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN `name` VARCHAR(256) COLLATE utf8mb4_unicode_ci;
