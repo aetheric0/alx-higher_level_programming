ea054804ed903787be5fc5eef5c4ac3af2bb47f3
Monarch <monarchofaether@gmail.com>
2024-06-17 15:47:23 +0300

    trim added for 6-multi_language_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const sentences = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const sentence of sentences) {
-  console.log(sentence);
+  console.log(sentence.trim());
 }
