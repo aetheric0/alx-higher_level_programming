e806f2a8acacee5b7331bdad9ac103fb8c26e237
Monarch <monarchofaether@gmail.com>
2024-06-17 16:19:11 +0300

    factorial using js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+let result = 1;
+const number = Math.floor(Number(process.argv[2]));
+function factorial (number) {
+  if (isNaN(number)) {
+    console.log(1);
+    return;
+  }
+  if (number === 0) {
+    console.log(result);
+    return;
+  }
+  result *= number;
+  return factorial(number - 1);
+}
+factorial(number);
