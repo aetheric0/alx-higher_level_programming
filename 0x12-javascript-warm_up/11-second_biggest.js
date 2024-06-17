d8f8542be2957e3d30a9ef7b22bf8c1022a407e4
Monarch <monarchofaether@gmail.com>
2024-06-17 17:04:08 +0300

    conditions added

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,15 +4,19 @@ let result;
 process.argv.slice(2).forEach(val => {
   values.push(Math.floor(Number(val)));
 });
-function biggestValue() {
-  result = values[0];
-  for (let i = 0; i < values.length; i++) {
-    if (values[i] > result) {
-      result = values[i]
+if (values.length < 2) {
+  console.log(0);
+} else {
+    function biggestValue () {
+      result = values[0];
+    for (let i = 0; i < values.length; i++) {
+      if (values[i] > result) {
+        result = values[i];
+      }
     }
   }
+  biggestValue();
+  values = values.filter(value => value !== result);
+  biggestValue();
+  console.log(result);
 }
-biggestValue();
-values = values.filter(value => value !== result);
-biggestValue();
-console.log(result);
