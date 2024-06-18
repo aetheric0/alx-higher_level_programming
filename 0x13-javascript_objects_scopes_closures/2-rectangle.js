d0a9c5f920d360d843bcaa2f02e97a4c4cca71eb
Monarch <monarchofaether@gmail.com>
2024-06-18 12:40:29 +0300

    isNaN condition added, semi-standardized

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      // Intentionally empty
     } else {
       this.width = w;
       this.height = h;
