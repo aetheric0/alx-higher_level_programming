f55b45d02826d72af819a11657008e6f16698938
Monarch <monarchofaether@gmail.com>
2024-06-21 13:57:09 +0300

    syntax error corrected

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    engine = MySQLdb.connect(host='localhost' port='3306',
+    engine = MySQLdb.connect(host='localhost', port='3306',
                              user=argv[1], password=argv[2], db=argv[3])
     c = engine.cursor()
     c.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY ASC')
