2170b50cf1d328a48dd6842a14106a2c392ea8ce
Monarch <monarchofaether@gmail.com>
2024-06-21 12:22:31 +0300

    sql syntax corrected

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3])
     c = db.cursor()
-    c.execute('SELECT * FROM states WHERE name LIKE "N% ORDER BY id ASC"')
+    c.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
     for row in c.fetchall():
         print(row)
 
