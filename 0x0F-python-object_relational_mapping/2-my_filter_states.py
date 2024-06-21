76f7d103564654979ef68ecd3a939c7a1d38ee3a
Monarch <monarchofaether@gmail.com>
2024-06-21 12:29:01 +0300

    using keyword arguments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], password=argv[2], db=argv[3])
     c = db.cursor()
     c.execute('SELECT * from states WHERE name = %s', (argv[4],))
     for row in c.fetchall():
