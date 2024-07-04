bc3fb9639c16222e7c779a42e911e60d326ebf2a
Monarch <monarchofaether@gmail.com>
2024-07-04 18:45:32 +0300

    script made 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 # Gets the size of body of response
-socket=$1
-curl -sI "$socket" | grep -i 'Content-Length' | awk '{print $2}'
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
