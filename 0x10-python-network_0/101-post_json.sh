2d919a0dc62baab10ddeecda3b78f9af78ea14d9
Monarch <monarchofaether@gmail.com>
2024-07-04 21:01:23 +0300

    options separated

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Gets the body of the response
-curl -X POST -sH "Content-Type: application/json" --data-binary "@2" "$1"
+curl -s -X POST -H "Content-Type: application/json" --data-binary "@2" "$1"
