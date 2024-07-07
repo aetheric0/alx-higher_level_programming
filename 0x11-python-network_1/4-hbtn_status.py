704fd5251ba8fcfc6b9daf1a16cebd5751278860
Monarch <monarchofaether@gmail.com>
2024-07-07 16:12:27 +0300

    using request module to fetch data from url

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Uses requests modult to fetch url
+"""
+
+if __name__ == '__main__':
+    import requests
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print ('Body response:')
+    print('\t- type: {}\n\t- content: {}'.format(type(req.text), req.text))
