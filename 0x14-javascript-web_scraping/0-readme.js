6f88f1ad8b4a90086e102fd8f7316ebab8bdb30a
Monarch <monarchofaether@gmail.com>
2024-07-31 09:05:58 +0300

    semisemistandard fixed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-fs = require('fs');
+const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-    return;
-  }
-  else {
+  } else {
     console.log(data);
   }
 });
