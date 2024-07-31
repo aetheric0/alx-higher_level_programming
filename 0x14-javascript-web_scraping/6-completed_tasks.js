1e2f4dffb97f9f822a3a555dbb6d20466629f7ee
Monarch <monarchofaether@gmail.com>
2024-07-31 11:52:15 +0300

    semistandardized

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const newData = {};
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
@@ -9,11 +8,11 @@ request(url, function (error, response, body) {
   const data = JSON.parse(body);
   const completedTasks = {};
   for (const task of data) {
-      if (task.completed) {
-        if (!completedTasks[task.userId]) {
-          completedTasks[task.userId] = 0
-        }
-        completedTasks[task.userId]++;
+    if (task.completed) {
+      if (!completedTasks[task.userId]) {
+        completedTasks[task.userId] = 0;
+      }
+      completedTasks[task.userId]++;
     }
   }
   console.log(completedTasks);
