66b0f8893b4033e8a1dc6ec4756d3762cd888dd7
Monarch <monarchofaether@gmail.com>
2024-07-31 11:06:45 +0300

    found number of movies by character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let characterFilms = [];
+let foundCharacter = false;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const films = JSON.parse(body).results;
+  for (const film of films) {
+    for (const character of film.characters) {
+      if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        request(character, function (error, response, body) {
+          if (error) {
+            console.error(error);
+          }
+          const characterData = JSON.parse(body);
+          characterFilms = characterData.films
+          console.log(characterFilms.length)
+          return;
+        });
+        foundCharacter = true;
+        break;
+      }
+    }
+    if (foundCharacter) {
+      break;
+    }
+  }
+});
